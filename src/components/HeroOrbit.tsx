cc72e17e991257ff3975ee92689578b414fd4b1d
Sameh <sameh.abdelhaleem95@gmail.com>
2024-10-20 16:31:17 +0300

    add and postion sparkle icons

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -8,14 +8,19 @@ export const HeroOrbit = ({
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className="border border-red-500 "
+        className=" "
         style={{
-          transform: `rotate(${rotation})`,
+          transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
           width: `${size}px`,
         }}
       >
-        <div className="border border-red-500 inline-flex ">{children}</div>
+        <div
+          className=" inline-flex "
+          style={{ transform: `rotate(${rotation * -1}deg)` }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
